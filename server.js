5aa14ceace6878c509157f7cecc92ea445306422
Jordan Beth <jordanbeth14@gmail.com>
2019-03-12 18:43:34 -0400

    add more

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 4200;
+
+app.use(express.static(__dirname + '/dist/Clueless'));
+
+app.get("/*", (request, response) => {
+  response.sendFile(path.join(__dirname));
+});
+
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Visit localhost:${PORT}`);
+});
+
+const io = require("socket.io").listen(server);
+
+/**
+ * Socket events
+ */
+io.sockets.on('connection', function (socket) {
+  console.log('Socket connected');
+  
+});
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { NgModule } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
